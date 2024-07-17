403d3fba1dd7194af93ee6ebc0fed82d79b6012c
meethatkar <meethatkar000@gmail.com>
2024-07-17 11:00:47 +0530

    added optional input & toggle button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatSlideToggleModule,MatAutocompleteModule,
+      MatInputModule,ReactiveFormsModule,FormsModule,MatFormFieldModule], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'trialApp';
+  myControl = new FormControl('');
+  options: string[] = ['One','Two','Three'];
 }
